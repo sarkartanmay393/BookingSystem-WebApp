09f2f455bda48a8451e6de81f59a53a9a94e8180
Tanmay Sarkar <84321236+sarkartanmay393@users.noreply.github.com>
2022-10-09 16:21:04 +0530

    Remove unneccessary obbjects.
    Signed-off-by: Tanmay Sarkar <84321236+sarkartanmay393@users.noreply.github.com>

--- internal/models/tempateDate.go
+++ internal/models/tempateDate.go
@@ -1,7 +1,6 @@
 package models
 
 import (
-	"github.com/sarkartanmay393/RoomReservation-WebApp/internal/config"
 	"github.com/sarkartanmay393/RoomReservation-WebApp/internal/form"
 )
 
@@ -19,10 +18,4 @@ type TemplateData struct {
 	SuccessMessage string
 	ErrorMessage   string
 	WarningMessage string
-}
-
-var appConfig *config.AppConfig
-
-func AttachConfigToTemplateData(appCon *config.AppConfig) {
-	appConfig = appCon
 }
